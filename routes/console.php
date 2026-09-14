9673af5017bba7d6b9fdd81646e355a4567f13d6
gouravatced-ops <anitagourav7668@gmail.com>
2026-09-14 14:47:15 +0530

    Update console.php

--- routes/console.php
+++ routes/console.php
@@ -9,3 +9,4 @@ Artisan::command('inspire', function () {
 })->purpose('Display an inspiring quote');
 
 Schedule::command('app:send-application-due-reminders')->dailyAt('06:00');
+Schedule::command('app:auto-escalate-stalled-applications --days=3')->dailyAt('06:05');
