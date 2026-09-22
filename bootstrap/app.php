7784aeab766ae297d88b1a3942ca1716ac44067a
gouravatced-ops <anitagourav7668@gmail.com>
2026-09-22 11:32:37 +0530

    Changes for Security Header

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,6 +17,7 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
         
         $middleware->append(\App\Http\Middleware\DecryptRouteParameters::class);
+        $middleware->append(\App\Http\Middleware\SecurityHeaders::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->render(function (\Illuminate\Session\TokenMismatchException $e, \Illuminate\Http\Request $request) {
